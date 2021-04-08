ea9824dce7862f28f070814dfdc25258af08d696
MD Arif Uddin <78405318+mdarifuddin1@users.noreply.github.com>
2021-04-08 14:30:54 +0600

    Add files via upload
    User input two  integer sum

---
+++ user input two intger and sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main (){
+
+    int num1,num2,sum;
+
+    printf("Enter two integer Number");
+
+    scanf("%d %d", &num1,&num2);
+
+    sum = num1 + num2;
+
+    printf("The sum of the two integer %d", sum);
+
+    return 0;
+}
